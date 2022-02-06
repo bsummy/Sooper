bbbdc4e90e3fa4139171323cff2135b4ecc4b341
Bennett Summy <bennettsummy@gmail.com>
2022-02-06 08:56:02 -0500

    Attempt to split out the CSS and JS with Cross

---
+++ src/Cross.js
@@ -0,0 +1,13 @@
+import React, {}  from 'react';
+
+
+function Cross() {
+    return (
+      <div classname = "Cross">
+        <p classname="Cross.left" style="text-align:left"> X </p>
+        <p classname="Cross.right" style="text-align:right"> X </p>
+      </div>
+    );
+
+}
+  export default Cross;
